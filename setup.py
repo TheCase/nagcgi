b0b6c4013d5a10192012da5784ab0a7245c848fc
TheCase <bryan@caseyclan.net>
2017-04-08 17:02:22 -0600

    add setup file for pip+ builds

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='nagcgi',
+      version='1.0.1',
+      description="Python bindings to Nagios' cmd.cgi",
+      author="John Morrissey",
+      author_email="jwm@horde.net",
+      url="https://github.com/jwm/nagcgi",
+      py_modules=['nagcgi']
+      )
